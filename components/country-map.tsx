5d7926eaa96b910663a870cbae27a4e633c5adf8
Tanay Nistala <t.nistala@icloud.com>
2022-08-17 21:07:24 -0400

    Fixed country map spacing

--- components/country-map.tsx
+++ components/country-map.tsx
@@ -27,57 +27,55 @@ const CountryMap = ({ code, scale, level = 1 }: Props) => {
 
   return (
     <div>
-      <div>
-        <ComposableMap
-          projection="geoAzimuthalEqualArea"
-          width={mapWidth}
-          height={mapHeight}
-          className="rounded-lg"
-          data-tip=""
-          projectionConfig={{
-            rotate: [-country.coordinates[0], -country.coordinates[1], 0],
-            // center: country.coordinates,
-            scale,
-          }}
+      <ComposableMap
+        projection="geoAzimuthalEqualArea"
+        width={mapWidth}
+        height={mapHeight}
+        className="rounded-lg mt-12"
+        data-tip=""
+        projectionConfig={{
+          rotate: [-country.coordinates[0], -country.coordinates[1], 0],
+          // center: country.coordinates,
+          scale,
+        }}
+      >
+        <ZoomableGroup
+          translateExtent={[
+            [0, 0],
+            [mapWidth, mapHeight],
+          ]}
         >
-          <ZoomableGroup
-            translateExtent={[
-              [0, 0],
-              [mapWidth, mapHeight],
-            ]}
+          <Sphere className="fill-gray-100" />
+          <Geographies
+            geography={`https://raw.githubusercontent.com/piwodlaiwo/TopoJSON-Data/master/diva-gis/${country.code3}_adm/${country.code3}_adm${level}.topo.json`}
           >
-            <Sphere className="fill-gray-100" />
-            <Geographies
-              geography={`https://raw.githubusercontent.com/piwodlaiwo/TopoJSON-Data/master/diva-gis/${country.code3}_adm/${country.code3}_adm${level}.topo.json`}
-            >
-              {({ geographies }) =>
-                geographies.map((feature) => (
-                  <Geography
-                    className="outline-none fill-gray-300 stroke-gray-400 stroke-1 hover:fill-gray-400"
-                    key={feature.rsmKey}
-                    geography={feature}
-                    onMouseEnter={() => {
-                      setTooltipContent(
-                        `${
-                          level == 1
-                            ? feature.properties.NAME_1
-                            : feature.properties.NAME_2
-                        }`
-                      );
-                    }}
-                    onMouseLeave={() => {
-                      setTooltipContent("");
-                    }}
-                  />
-                ))
-              }
-            </Geographies>
-          </ZoomableGroup>
-        </ComposableMap>
-        <ReactTooltip className="font-mono font-bold text-8xl bg-blue-200 text-blue-500 shadow-lg">
-          {tooltipContent}
-        </ReactTooltip>
-      </div>
+            {({ geographies }) =>
+              geographies.map((feature) => (
+                <Geography
+                  className="outline-none fill-gray-300 stroke-gray-400 stroke-1 hover:fill-gray-400"
+                  key={feature.rsmKey}
+                  geography={feature}
+                  onMouseEnter={() => {
+                    setTooltipContent(
+                      `${
+                        level == 1
+                          ? feature.properties.NAME_1
+                          : feature.properties.NAME_2
+                      }`
+                    );
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+      <ReactTooltip className="font-mono font-bold text-8xl bg-blue-200 text-blue-500 shadow-lg">
+        {tooltipContent}
+      </ReactTooltip>
     </div>
   );
 };
