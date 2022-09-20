9a1f69d474e11760436228e4f7f503e860e6c3ef
Tanay Nistala <tanaynistala@tanays-macbook-pro.local>
2022-09-20 15:55:35 +0000

    Cleaned up some files

--- components/page-components/road-markings.tsx
+++ components/page-components/road-markings.tsx
@@ -1,9 +1,8 @@
 type Props = {
   children?: React.ReactNode | React.ReactNode[]
-  bg: string
 }
 
-const RoadMarkings = ({ children, bg = "black" }: Props) => {
+const RoadMarkings = ({ children }: Props) => {
   return <div className="bg-gray-700 space-y-2 py-2">{children}</div>
 }
 
